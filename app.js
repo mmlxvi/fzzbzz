aef4d71bf4cf04eb039a1f305364fb3bd425ca57
mmlxvi <quarterly24@gmail.com>
2014-09-26 14:30:20 -0500

    adds

--- app.js
+++ app.js
@@ -5,45 +5,42 @@ var limit;
 	
 $('#button').click(function() {
 
+
 	   //if(e.keyCode == 13) {
 		
     	limit = document.getElementById("numberInput").value;
-    	
     	$('#numberInput').val('');
 
+    	if (limit < 1 || limit > 100) {
+    		alert("Please enter a number between 1 and 100.");
+    	}
+    	
+    	else if (isNaN(limit)) {
+    		alert("\"" + limit + "\"" + " is not a number. Please enter a number between 1 and 100.");
+    	}
 
-	//}
+    	else {
 
-/* $('#numberInput').keyup(function(e) {
-
-		if(e.keyCode == 13) {
-			alert("key up");
-		}
-}) */
-	
-
-
-
-
-	for (counter = 1; counter <= limit; counter++) {
-    if ((counter % 3 === 0) && (counter % 5 === 0)) {
-        //console.log("fizzbuzz");
-        $("#listonumbers").append('<p>' + "fizzbuzz" + '</p>');
-    }
+			for (counter = 1; counter <= limit; counter++) {
+    		if ((counter % 3 === 0) && (counter % 5 === 0)) {
+        	//console.log("fizzbuzz");
+        	$("#listonumbers").append('<p>' + "fizzbuzz" + '</p>');
+    		}
     
-    else if (counter % 3 === 0) {
-        //console.log("fizz");
-        $("#listonumbers").append('<p>' + "fizz" + '</p>');
-    }
+    		else if (counter % 3 === 0) {
+        	//console.log("fizz");
+        	$("#listonumbers").append('<p>' + "fizz" + '</p>');
+    		}
     
-    else if (counter % 5 === 0) {
-        //console.log("buzz");
-        $("#listonumbers").append('<p>' + "buzz" + '</p>');
-    }
-    else {
-        //console.log(counter);
-        $("#listonumbers").append('<p>' + counter + '</p>');
-    }
+    		else if (counter % 5 === 0) {
+        	//console.log("buzz");
+        	$("#listonumbers").append('<p>' + "buzz" + '</p>');
+   			}
+    		else {
+        	//console.log(counter);
+        	$("#listonumbers").append('<p>' + counter + '</p>');
+    		}
+    	}
 }
 	
 })
